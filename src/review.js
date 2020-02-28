10b8930e30a84d423eee1cb792f1be6eca2ca36f
Caruki <caruki91@posteo.de>
2020-02-28 11:33:13 +0100

    Add css classes to variables

--- src/review.js
+++ src/review.js
@@ -3,6 +3,8 @@ function addReview() {
   let review = document.createElement("div");
   let reviewText = document.createElement("p");
   let reviewTextContent = document.createTextNode("Test test test");
+  review.classList = "review";
+  reviewText.classList = "text review-text";
   reviewText.appendChild(reviewTextContent);
   review.appendChild(reviewText);
   reviewSlider.appendChild(review);
